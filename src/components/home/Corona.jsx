a122d8c1ec49197a0bda73b6d08731b375174c37
Dominic Elm <elmdominic@gmx.net>
2020-04-26 22:10:25 +0200

    fix(corona): remove date

--- src/components/home/Corona.jsx
+++ src/components/home/Corona.jsx
@@ -25,9 +25,9 @@ export default () => (
     <div>
       <strong>Wir sind weiterhin online für Euch da!</strong>
       <br />
-      Da der Unterricht in unserem Yoga-Studio mindestens bis zum 02.05.2020 leider nicht möglich ist, sind wir aktuell
-      mit einem Online-Kursangebot für Euch da. Wir folgen somit den Empfehlungen unserer Bundesregierung und möchten
-      weiterhin diejenigen schützen, die aktuell besonders gefährdet sind.
+      Da der Unterricht in unserem Yoga-Studio bis auf weiteres leider nicht möglich ist, sind wir aktuell mit einem
+      Online-Kursangebot für Euch da. Wir folgen somit den Empfehlungen unserer Bundesregierung und möchten weiterhin
+      diejenigen schützen, die aktuell besonders gefährdet sind.
       <br />
       <br />
       Dennoch möchten wir Euch dabei helfen auch zuhause eine strukturierte Übungspraxis zu etablieren und Euch
