826fb280fa29dc6d02bd9cf5f0f94a221e2c33c3
Dominic Elm <elmdominic@gmx.net>
2022-03-21 12:11:54 +0100

    feat: re-design schedule

--- src/components/home/Schedule.jsx
+++ src/components/home/Schedule.jsx
@@ -2,7 +2,7 @@ import { graphql, StaticQuery } from 'gatsby';
 import React, { Component } from 'react';
 import Scroll from 'react-scroll';
 import styled from 'styled-components';
-import { mb, ml, mr, mt, p } from 'styled-components-spacing';
+import { mb, mt, p } from 'styled-components-spacing';
 import { extractNodes } from '../../utils';
 import { PrimaryButton, SectionHeader } from '../styled/Base';
 import { Container, StretchContainer } from '../styled/Layout';
@@ -21,18 +21,18 @@ const ScheduleHeader = styled.div`
 
 const Day = styled.div`
   padding: 0;
+
+  ${breakpoint('md-up')} {
+    flex: 1;
+  }
 `;
 
 const Cell = styled.div`
   ${p(2)}
   ${flexCenter()}
 
-  ${breakpoint('sm')} {
-    height: 50px;
-  }
-
-  ${breakpoint('md')} {
-    height: 100px;
+  ${breakpoint('md-up')} {
+    height: 120px;
   }
 
   ${breakpoint('lg-up')} {
@@ -45,21 +45,53 @@ export const closed = () => {
 };
 
 const Course = styled(StretchContainer)`
-  ${flexCenter()};
+  display: flex;
+  flex-direction: column;
   background: ${props => props.theme.colors.white};
+  border-radius: 4px 4px 0 0;
 
   ${closed()} {
     opacity: 0.8;
   }
+`;
+
+const CourseContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${flexCenter()}
+  flex: 1;
+  border: 1px solid ${props => props.theme.colors.border};
 
   ${breakpoint('sm')} {
-    flex-direction: row;
+    ${p(2)};
   }
 
-  ${breakpoint('md-up')} {
-    border: 1px solid ${props => props.theme.colors.border};
-    flex-direction: column;
-  }
+  ${({ online }) => {
+    if (!online) {
+      return `
+        ${breakpoint('md-up')} {
+          padding-top: 10px;
+          margin-top: 10px;
+        }
+
+        ${breakpoint('md')} {
+          padding-top: 16px;
+          padding-bottom: 6px;
+        }
+      `;
+    }
+
+    return `
+      border-top: 1px solid transparent;
+
+      ${breakpoint('sm')} {
+      }
+
+      ${breakpoint('md')} {
+        padding: 6px 0;
+      }
+    `;
+  }}
 `;
 
 const CourseTitle = styled.h4`
@@ -75,8 +107,12 @@ const CourseTitle = styled.h4`
     margin: 0;
   }
 
-  ${breakpoint('lg-up')} {
+  ${breakpoint('md')} {
     font-size: 18px;
+  }
+
+  ${breakpoint('lg-up')} {
+    font-size: 20px;
   }
 `;
 
@@ -86,46 +122,25 @@ const CourseTrainer = styled.span`
   color: #929191;
 `;
 
-const TimeColumn = styled.div`
-  ${breakpoint('sm')} {
-    width: 130px;
-    max-width: 130px;
-  }
-`;
+const CourseTime = styled(CourseTrainer)``;
 
-const OnlineBadge = styled.div`
+const Badge = styled.div`
   background: ${props => props.theme.colors.accent};
-  color: white;
-  border-radius: 4px;
-  padding: 3px 6px 1px;
+  color: ${props => props.theme.colors.white};
+  border-radius: 4px 4px 0 0;
+  text-transform: uppercase;
+  width: 100%;
+  padding: 5px 6px 3px;
+  text-align: center;
   font-size: 12px;
   line-height: 1;
-  margin: 0;
-
-  ${breakpoint('sm')} {
-    padding: 3px 6px 2px;
-    ${mr(2)}
-    ${mb(1)}
-  }
-
-  ${breakpoint('md-up')} {
-    ${mb(2)}
-  }
 `;
 
 const Closed = styled.h5`
   margin: 0;
   font-weight: 500;
-  font-size: 14px;
+  font-size: 16px;
   color: #929191;
-
-  ${breakpoint('sm')} {
-    ${ml(2)}
-  }
-
-  ${breakpoint('md-up')} {
-    ${mt(1)}
-  }
 `;
 
 const ContentColumn = ({ className, day }) => {
@@ -134,40 +149,58 @@ const ContentColumn = ({ className, day }) => {
   return (
     <div className={className} name="schedule">
       <ScheduleHeader>{day.name}</ScheduleHeader>
-      {courses.map((course, courseIndex) => {
-        if (course) {
-          const closed = course.closed;
-          const online = course.online;
-          const classes = closed ? 'closed' : '';
+      {courses
+        .filter(course => course != null)
+        .sort((a, b) => {
+          const timeA = a.time;
+          const timeB = b.time;
 
-          return (
-            <Cell key={courseIndex}>
-              <Course className={classes}>
-                {online ? <OnlineBadge>online</OnlineBadge> : null}
-                <CourseTitle className={classes}>{course.title}</CourseTitle>
-                {closed ? (
-                  <Closed>GESCHLOSSEN</Closed>
-                ) : (
-                  <CourseTrainers className="d-none d-md-block">
-                    {course.trainers.map((trainer, trainerIndex) => {
-                      const isLast = trainerIndex === course.trainers.length - 1;
+          if (timeA < timeB) {
+            return -1;
+          }
 
-                      return (
-                        <CourseTrainer key={trainerIndex}>
-                          {trainer.name}
-                          {!isLast ? ', ' : ''}
-                        </CourseTrainer>
-                      );
-                    })}
-                  </CourseTrainers>
-                )}
-              </Course>
-            </Cell>
-          );
-        }
+          if (timeA > timeB) {
+            return 1;
+          }
 
-        return <Cell key={courseIndex} />;
-      })}
+          return 0;
+        })
+        .map((course, courseIndex) => {
+          if (course) {
+            const { time, online, closed } = course;
+            const classes = closed ? 'closed' : '';
+
+            return (
+              <Cell key={courseIndex}>
+                <Course className={classes} online={online}>
+                  {online ? <Badge>online</Badge> : null}
+                  <CourseContent online={online}>
+                    <CourseTime className="mb-1">{time}</CourseTime>
+                    <CourseTitle className={classes}>{course.title}</CourseTitle>
+                    {closed ? (
+                      <Closed>GESCHLOSSEN</Closed>
+                    ) : (
+                      <CourseTrainers>
+                        {course.trainers.map((trainer, trainerIndex) => {
+                          const isLast = trainerIndex === course.trainers.length - 1;
+
+                          return (
+                            <CourseTrainer key={trainerIndex}>
+                              {trainer}
+                              {!isLast ? ', ' : ''}
+                            </CourseTrainer>
+                          );
+                        })}
+                      </CourseTrainers>
+                    )}
+                  </CourseContent>
+                </Course>
+              </Cell>
+            );
+          }
+
+          return <Cell key={courseIndex} />;
+        })}
     </div>
   );
 };
@@ -217,20 +250,12 @@ export default class Schedule extends Component {
     selectedFilter: 0
   };
 
-  createCourse(timeslot, day) {
-    const course = day[`course_${timeslot}`];
-
-    if (course) {
-      return {
-        title: course.title,
-        trainers: day[`trainer_${timeslot}`],
-        online: day[`online_${timeslot}`],
-        closed: day[`closed_${timeslot}`]
-      };
-    }
-
-    return null;
-  }
+  createSchedule = days => {
+    return days.reduce((acc, day) => {
+      acc[day] = { name: day, courses: [] };
+      return acc;
+    }, {});
+  };
 
   scrollToSection = section => {
     scroll.scrollTo(section, {
@@ -257,57 +282,27 @@ export default class Schedule extends Component {
                 }
               }
             }
-            allContentfulDay {
+            allContentfulCourseEntry {
               edges {
                 node {
-                  name
-                  course_09001030 {
+                  day {
+                    name
+                  }
+                  course {
                     title
                   }
-                  online_09001030
-                  closed_09001030
-                  trainer_09001030 {
-                    name: firstname
+                  trainers {
+                    firstname
                   }
-                  course_15301700 {
-                    title
-                  }
-                  online_15301700
-                  closed_15301700
-                  trainer_15301700 {
-                    name: firstname
-                  }
-                  course_17301900 {
-                    title
-                  }
-                  online_17301900
-                  closed_17301900
-                  trainer_17301900 {
-                    name: firstname
-                  }
-                  course_18302000 {
-                    title
-                  }
-                  online_18302000
-                  closed_18302000
-                  trainer_18302000 {
-                    name: firstname
-                  }
-                  course_19302100 {
-                    title
-                  }
-                  online_19302100
-                  closed_19302100
-                  trainer_19302100 {
-                    name: firstname
-                  }
+                  time
+                  online
+                  closed
                 }
               }
             }
           }
         `}
-        render={({ allContentfulDay, allContentfulCourse }) => {
-          const slots = ['09001030', '15301700', '17301900', '18302000', '19302100'];
+        render={({ allContentfulCourseEntry, allContentfulCourse }) => {
           const filters = ['Alle'].concat(
             extractNodes(allContentfulCourse).map(course => course.title),
             ['Online']
@@ -316,13 +311,20 @@ export default class Schedule extends Component {
           const { selectedFilter } = this.state;
           const filter = filters[selectedFilter];
 
-          const schedule = extractNodes(allContentfulDay).reduce((acc, day) => {
-            acc[day.name] = {
-              name: day.name,
-              courses: slots.map(timeslot => this.createCourse(timeslot, day))
-            };
-            return acc;
-          }, {});
+          const schedule = this.createSchedule(['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']);
+
+          extractNodes(allContentfulCourseEntry).forEach(entry => {
+            const day = entry.day.name;
+            const { course, trainers, time, online, closed } = entry;
+
+            schedule[day].courses.push({
+              title: course.title,
+              trainers: trainers.map(trainer => trainer.firstname),
+              time,
+              online,
+              closed
+            });
+          });
 
           const filteredSchedule = Object.keys(schedule).reduce((filteredSchedule, day) => {
             filteredSchedule[day] = filterDay(schedule[day], filter);
@@ -344,69 +346,29 @@ export default class Schedule extends Component {
                 ))}
               </Filters>
               <div className="row p-1 p-md-0">
-                <Day className="col-sm-12 col-md-4 pl-1">
+                <Day className="col-sm-12 mb-4 mb-md-0">
                   <div className="row no-gutters">
-                    <TimeColumn className="col col-lg-6">
-                      <ScheduleHeader>Zeit</ScheduleHeader>
-                      <Cell>09:00 - 10:30</Cell>
-                      <Cell>15:30 - 17:00</Cell>
-                      <Cell>17:30 - 19:00</Cell>
-                      <Cell>18:30 - 20:00</Cell>
-                      <Cell>19:30 - 21:00</Cell>
-                    </TimeColumn>
-                    <ContentColumn className="col col-lg-6" day={filteredSchedule['Montag']} />
+                    <ContentColumn className="col pb-1 pb-md-0" day={filteredSchedule['Montag']} />
                   </div>
                 </Day>
-                <Day className="col-sm-12 col-md-2">
+                <Day className="col-sm-12 mb-4 mb-md-0">
                   <div className="row no-gutters">
-                    <TimeColumn className="d-md-none">
-                      <ScheduleHeader>Zeit</ScheduleHeader>
-                      <Cell>09:00 - 10:30</Cell>
-                      <Cell>15:30 - 17:00</Cell>
-                      <Cell>17:30 - 19:00</Cell>
-                      <Cell>18:30 - 20:00</Cell>
-                      <Cell>19:30 - 21:00</Cell>
-                    </TimeColumn>
-                    <ContentColumn className="col col-lg-12" day={filteredSchedule['Dienstag']} />
+                    <ContentColumn className="col pb-1 pb-md-0" day={filteredSchedule['Dienstag']} />
                   </div>
                 </Day>
-                <Day className="col-sm-12 col-md-2">
+                <Day className="col-sm-12 mb-4 mb-md-0">
                   <div className="row no-gutters">
-                    <TimeColumn className="d-md-none">
-                      <ScheduleHeader>Zeit</ScheduleHeader>
-                      <Cell>09:00 - 10:30</Cell>
-                      <Cell>15:30 - 17:00</Cell>
-                      <Cell>17:30 - 19:00</Cell>
-                      <Cell>18:30 - 20:00</Cell>
-                      <Cell>19:30 - 21:00</Cell>
-                    </TimeColumn>
-                    <ContentColumn className="col col-lg-12" day={filteredSchedule['Mittwoch']} />
+                    <ContentColumn className="col pb-1 pb-md-0" day={filteredSchedule['Mittwoch']} />
                   </div>
                 </Day>
-                <Day className="col-sm-12 col-md-2">
+                <Day className="col-sm-12 mb-4 mb-md-0">
                   <div className="row no-gutters">
-                    <TimeColumn className="d-md-none">
-                      <ScheduleHeader>Zeit</ScheduleHeader>
-                      <Cell>09:00 - 10:30</Cell>
-                      <Cell>15:30 - 17:00</Cell>
-                      <Cell>17:30 - 19:00</Cell>
-                      <Cell>18:30 - 20:00</Cell>
-                      <Cell>19:30 - 21:00</Cell>
-                    </TimeColumn>
-                    <ContentColumn className="col col-lg-12" day={filteredSchedule['Donnerstag']} />
+                    <ContentColumn className="col pb-1 pb-md-0" day={filteredSchedule['Donnerstag']} />
                   </div>
                 </Day>
-                <Day className="col-sm-12 col-md-2 pr-1">
+                <Day className="col-sm-12">
                   <div className="row no-gutters">
-                    <TimeColumn className="d-md-none">
-                      <ScheduleHeader>Zeit</ScheduleHeader>
-                      <Cell>09:00 - 10:30</Cell>
-                      <Cell>15:30 - 17:00</Cell>
-                      <Cell>17:30 - 19:00</Cell>
-                      <Cell>18:30 - 20:00</Cell>
-                      <Cell>19:30 - 21:00</Cell>
-                    </TimeColumn>
-                    <ContentColumn className="col col-lg-12" day={filteredSchedule['Freitag']} />
+                    <ContentColumn className="col pb-1 pb-md-0" day={filteredSchedule['Freitag']} />
                   </div>
                 </Day>
               </div>
